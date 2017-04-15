61507da6b4ff6e0bfee8a51e03a740b50665f59a
Kyle <kylelinwu@gmail.com>
2017-04-15 10:24:04 -0700

    Added sketchpad + Resize Button

---
+++ js/sketch.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  defaultGrid();
+  $('.drawBox').hover(function(){
+    $(this).addClass('drawnOver');
+  });
+  $('button').click(function(){
+    var newGridSize = prompt('How many boxes should be on each side?');
+    if (isNaN(parseInt(newGridSize)) === false){
+      $('table').empty();
+      resizeGrid(newGridSize);
+    }else{
+      window.alert("That's not a number! Try again with a real number this time.");
+    }
+  });
+  $(document).on('mouseenter', '.drawBox',function(){
+      $(this).addClass('drawnOver');
+  });
+});
+
+addRow = function(rows){
+  for(i=0;i<rows;i++){
+  $('table').append('<tr></tr>');
+};
+}
+addColumn = function(columns){
+  for(i=0; i<columns; i++){
+    $('tr').append("<td class='drawBox'><div></div></td>");
+  };
+}
+
+resizeGrid = function(boxes){
+  addRow(boxes);
+  addColumn(boxes);
+}
+defaultGrid = function(){
+  addRow(16);
+  addColumn(16);
+}
